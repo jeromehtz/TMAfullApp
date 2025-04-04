8cb7948eecea0e4f13978e94c1975aa62846e661
jeromehtz <rejome.htz@gmail.com>
2025-04-04 13:08:42 +0200

    https://github.com/jeromehtz/TMAfullApp/issues/9

--- script.js
+++ script.js
@@ -3,54 +3,55 @@ document.addEventListener('DOMContentLoaded', () => {
     const historique = document.getElementById('historique');
     const demandesValidation = document.getElementById('demandesValidation');
 
+    const raisonSelect = document.getElementById('raison');
+    const autreRaison = document.getElementById('autreRaison');
+
+    if (raisonSelect) {
+        raisonSelect.addEventListener('change', function () {
+            if (raisonSelect.value === 'autre') {
+                autreRaison.style.display = 'block';
+            } else {
+                autreRaison.style.display = 'none';
+            }
+        });
+    }
+
+
     if (form) {
         form.addEventListener('submit', function(event) {
             event.preventDefault();
 
             const nom = document.getElementById('nom').value;
             const prenom = document.getElementById('prenom').value;
-            const prenom_ = document.getElementById('prenom_').value;
+            const nomusager = document.getElementById('nomusager').value;
             const dateDebut = document.getElementById('dateDebut').value;
             const dateFin = document.getElementById('dateFin').value;
             let raison = document.getElementById('raison').value;
-            let raison_ = document.getElementById('raison_');
-            
-            const dateDemande = new Date();
-            console.log(raison);
-            
-            if (raison === "Autre")
-            {
-                if (raison_ === null) {
-                    alert('Veuillez saisir la raison de votre demande.');
-                    return;
-                }
-                raison = document.getElementById('raison_').value;
-                
-                
+            const autreRaison = document.getElementById('autreRaison');
+            if (document.getElementById('autreRaison')) {
+                raison = autreRaison.value;
             }
-            
+            const dateDemande = new Date();
+
+
             const demande = {
                 nom,
                 prenom,
-                prenom_,
+                nomusager,
                 dateDebut,
                 dateFin,
                 raison,
                 statut: 'en attente'
             };
-            if (!nom || !prenom || !dateDebut || !dateFin || !raison) {
-                alert('Veuillez remplir tous les champs correctement.');
-                return;
-            }
-            else if (new Date(dateDebut) < dateDemande) {
-                alert('La date de début ne peut pas être dans le passé.');
+            if (new Date(dateDebut) < dateDemande) {
+                alert('La date de début doit etre valide.');
                 document.getElementById('dateDebut').value="";
                 return;
             }
             else if (new Date(dateFin) < dateDemande) {
                 document.getElementById('dateFin').value="";
-                alert('La date de fin ne peut pas être dans le passé.');
-                
+                alert('La date de fin doit etre valide.');
+
                 return;
             }
             else if (new Date(dateDebut) > new Date(dateFin)) {
@@ -87,8 +88,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 li.innerHTML = `
                     <strong>Nom:</strong> ${demande.nom}<br>
                     <strong>Prénom:</strong> ${demande.prenom}<br>
-                    <strong>Deuxième prénom:</strong> ${demande.prenom_}<br>
-                    <strongDate de début:</strong> ${demande.dateDebut}<br>
+                    <strong>Nom d'usage:</strong> ${demande.nomusager}<br>
+                    <strong>Date de début:</strong> ${demande.dateDebut}<br>
                     <strong>Date de fin:</strong> ${demande.dateFin}<br>
                     <strong>Raison:</strong> ${demande.raison}<br>
                     <strong>Statut:</strong> <span class="font-semibold ${getStatutClass(demande.statut)}">${demande.statut}</span>
@@ -107,6 +108,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 li.innerHTML = `
                     <div class="mb-4">
                         <strong>Nom:</strong> ${demande.nom}<br>
+                        <strong>Prénom:</strong> ${demande.prenom}<br>
+                        <strong>Nom d'usage:</strong> ${demande.nomusager}<br>
                         <strong>Date de début:</strong> ${demande.dateDebut}<br>
                         <strong>Date de fin:</strong> ${demande.dateFin}<br>
                         <strong>Raison:</strong> ${demande.raison}<br>
